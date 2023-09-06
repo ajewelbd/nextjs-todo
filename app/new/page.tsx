cb46cb09741545f6ccbf0fb6c880ea3c7c9946c1
Md. Ashraful Alam <ajewel@schertech.com>
2023-09-06 20:02:59 +0600

    feat: list and create form added

---
+++ app/new/page.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+export default function CreateTodo() {
+    return (
+        <div className="w-full max-w-xs">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="title"
+                    >
+                        Title
+                    </label>
+                    <input
+                        name="title"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="title"
+                        type="text"
+                        placeholder="Enter title here..."
+                    />
+                </div>
+
+                <div className="flex items-center justify-start gap-5">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button"
+                    >
+                        Add
+                    </button>
+                    <Link
+                        href="/"
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button"
+                    >
+                        Cancel
+                    </Link>
+                </div>
+            </form>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import Link from "next/link";
+import { prisma } from "./db";
+import TodoItem from "@/components/todoItem";
 
-export default function Home() {
-    return <>Hello</>;
+export default async function Home() {
+    const todos = await prisma.todo.findMany();
+    return (
+        <>
+            <div className="flex justify-between">
+                <h1 className="text-2xl">ToDo's</h1>
+                <Link
+                    href="/new"
+                    className="text-2xl border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+                >
+                    New
+                </Link>
+            </div>
+            <div className="flex flex-col gap-2">
+                {todos.map((todo) => {
+                    <TodoItem key={todo.id} />;
+                })}
+            </div>
+        </>
+    );
 }
