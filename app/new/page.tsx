e50a1fb0a537f3f1ad4a78a73af38753a8706457
Md. Ashraful Alam <ajewel@schertech.com>
2023-09-07 12:31:37 +0600

    feat: initial feature added

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,9 +1,33 @@
 import Link from "next/link";
+import { prisma } from "../db";
+import { redirect } from "next/navigation";
+
+const create = async (data: FormData) => {
+    "use server";
+
+    const title = data.get("title")?.valueOf();
+
+    // console.log(data);
+
+    if (typeof title !== "string") {
+        throw new Error("Invalid Title!");
+    }
+
+    if (title === "") {
+        throw new Error("Invalid Title!");
+    }
+
+    await prisma.todo.create({ data: { title, complete: false } });
+    redirect("/");
+};
 
 export default function CreateTodo() {
     return (
         <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form
+                action={create}
+                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            >
                 <div className="mb-4">
                     <label
                         className="block text-gray-700 text-sm font-bold mb-2"
@@ -23,7 +47,7 @@ export default function CreateTodo() {
                 <div className="flex items-center justify-start gap-5">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="button"
+                        type="submit"
                     >
                         Add
                     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 import Link from "next/link";
 import { prisma } from "./db";
 import TodoItem from "@/components/todoItem";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
     const todos = await prisma.todo.findMany();
+
+    const toggleComplete = async (id: string, complete: boolean) => {
+        "use server";
+
+        await prisma.todo.update({ where: { id }, data: { complete } });
+    };
+
+    const deleteToDo = async (id: string) => {
+        "use server";
+
+        await prisma.todo.delete({ where: { id } });
+
+        redirect("/");
+    };
+
     return (
         <>
             <div className="flex justify-between">
-                <h1 className="text-2xl">ToDo's</h1>
+                <h1 className="text-2xl">ToDos</h1>
                 <Link
                     href="/new"
                     className="text-2xl border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
@@ -16,9 +32,14 @@ export default async function Home() {
                 </Link>
             </div>
             <div className="flex flex-col gap-2">
-                {todos.map((todo) => {
-                    <TodoItem key={todo.id} />;
-                })}
+                {todos.map((todo) => (
+                    <TodoItem
+                        key={todo.id}
+                        {...todo}
+                        toggleComplete={toggleComplete}
+                        deleteToDo={deleteToDo}
+                    />
+                ))}
             </div>
         </>
     );
